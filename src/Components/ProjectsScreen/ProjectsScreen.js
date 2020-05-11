bea5223be93540bd7ed25a6abccd7f405fafb97b
SilentKun <xsilentchannelx@gmail.com>
2020-05-12 00:46:24 +0300

    #5: Implemented nav bar in Projects Screen

--- src/Components/ProjectsScreen/ProjectsScreen.js
+++ src/Components/ProjectsScreen/ProjectsScreen.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {View, Text, TouchableOpacity, ActivityIndicator, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
+import {DrawerActions} from 'react-navigation-drawer';
 import {LoginHelper} from '../Registration';
 import {LoginManager} from '../../Helpers';
 import {routes} from '../../Constants';
+import {AppNavigationBar, AppTouchableIcon} from '../UIKit';
 
 class ProjectsScreen extends Component {
     constructor(props) {
@@ -16,12 +18,26 @@ class ProjectsScreen extends Component {
         this.state = this.loginHelper.buildInitialState();
     }
 
+    onMenuPress = () => {
+        this.props.navigation.dispatch(DrawerActions.openDrawer());
+    };
+
     render() {
         const email = this.props.currentUser?.email;
         const {inProgress} = this.state;
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Hello {email}</Text>
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <AppNavigationBar style={styles.navigationBar}>
+                    <AppTouchableIcon
+                        style={styles.menuIcon}
+                        icon="ios-menu"
+                        onPress={this.onMenuPress}
+                    />
+                    <Text style={styles.title}>
+                        Projects
+                    </Text>
+                </AppNavigationBar>
+                <Text>Projects List</Text>
                 {inProgress && <ActivityIndicator style={{marginTop: 10}} />}
             </View>
         );
@@ -33,5 +49,22 @@ const mapStateToProps = ({currentUser}) => {
         currentUser,
     };
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    navigationBar: {
+        width: '100%',
+    },
+    title: {
+        marginLeft: 20,
+        fontSize: 17,
+        letterSpacing: 0.15,
+        color: '#FFF',
+    },
+    menuIcon: {
+    },
+});
 
 export default connect(mapStateToProps, null)(ProjectsScreen);
