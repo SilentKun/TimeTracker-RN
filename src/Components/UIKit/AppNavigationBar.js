23958a576fe8ddfd8d4e71b3a364365293b95eac
Игорь Томилин <ito@aiweapps.com>
2020-06-03 18:03:06 +0300

    fixed safe area ios

--- src/Components/UIKit/AppNavigationBar.js
+++ src/Components/UIKit/AppNavigationBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import SafeAreaView from 'react-native-safe-area-view';
 
 const AppNavigationBar = ({style, children}) => {
     return (
